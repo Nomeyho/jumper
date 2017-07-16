b390f5c5848e9500c88834a13583a3a45b59f1eb
Stéphane Vanberg <svanberg@student.ulg.ac.be>
2017-07-16 14:26:35 +0100

    Application

---
+++ core/src/io/github/nomeyho/jumper/Application.java
@@ -0,0 +1,6 @@
+package io.github.nomeyho.jumper;
+
+public abstract class Application {
+    public static final boolean DEBUG = true;
+    public static final String TAG = "Jumper";
+}
--- core/src/io/github/nomeyho/jumper/GameScreen.java
+++ core/src/io/github/nomeyho/jumper/GameScreen.java
@@ -8,13 +8,13 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class GameScreen extends ScreenAdapter {
+    public static final float WORLD_WIDTH = 768;
+    public static final float WORLD_HEIGHT = 1024;
 
-    private static final float WORLD_WIDTH = 480;
-    private static final float WORLD_HEIGHT = 640;
     private ShapeRenderer shapeRenderer;
     private Viewport viewport;
     private Camera camera;
@@ -36,8 +36,7 @@ public class GameScreen extends ScreenAdapter {
         this.camera.position.set(WORLD_WIDTH / 2, WORLD_HEIGHT / 2, 0);
         this.camera.update();
         // Viewport
-        // TODO use extend viewport
-        this.viewport = new FitViewport(WORLD_WIDTH, WORLD_HEIGHT, this.camera);
+        this.viewport = new ExtendViewport(WORLD_WIDTH, WORLD_HEIGHT, this.camera);
         // Grid
         this.shapeRenderer = new ShapeRenderer();
         // Batch
@@ -57,13 +56,17 @@ public class GameScreen extends ScreenAdapter {
     }
 
     private void draw() {
-        /*
         // Render new scene
         this.batch.setProjectionMatrix(this.camera.projection);
         this.batch.setTransformMatrix(this.camera.view);
         this.batch.begin();
             // TODO: draw images (textures) here
         this.batch.end();
-        */
+    }
+
+    @Override
+    public void dispose () {
+        this.batch.dispose();
+        this.shapeRenderer.dispose();
     }
 }
