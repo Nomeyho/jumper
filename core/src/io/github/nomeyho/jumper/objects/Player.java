c351ffd48336bcd2404e180a49443a529fdafe9b
EzpzCraft p-o <pov975@gmail.com>
2017-07-18 12:54:56 +0200

    Add Accel smoothing

--- core/src/io/github/nomeyho/jumper/objects/Player.java
+++ core/src/io/github/nomeyho/jumper/objects/Player.java
@@ -12,8 +12,9 @@ public class Player extends AbstractGameObject {
     public static final float HEIGHT = 180;
     public static final float SPEED_MAX_X = 2000;
     public static final float SPEED_MAX_Y = 50;
-    public static final float ACCELX = 8000;
+    public static final float ACCELX = 4000;
     public static final float ACCELY = -10;
+    public static final float ACCEL_SMOOTH_LEVEL = 1;
 
     private float direction = 0;
     private TextureRegion playerTexture;
@@ -34,27 +35,28 @@ public class Player extends AbstractGameObject {
             this.direction = -1;
 
         // Acceleration
-        if(this.speed.x < SPEED_MAX_X )
-            this.speed.x += ACCELX * delta;
-        if(this.speed.y < SPEED_MAX_Y)
-            this.speed.y += ACCELY * delta;
-
-        // Move along X
-        if( Math.abs(this.touchedPos.x - this.location.getX() - WIDTH/2) > Math.abs(this.speed.x * delta / 2)) {
-            Float normWidth = MathUtils.clamp(
-                    this.location.getX() + ( direction * this.speed.x * delta),
-                    0,
-                    Application.worldWidth - Player.WIDTH
-            );
-            this.location.setLocation(normWidth, this.location.getY());
+        for(int n=1; n<=ACCEL_SMOOTH_LEVEL; n++)
+        {
+            if(this.speed.x < SPEED_MAX_X )
+                this.speed.x += ACCELX * delta / ACCEL_SMOOTH_LEVEL;
+            if(this.speed.y < SPEED_MAX_Y)
+                this.speed.y += ACCELY * delta / ACCEL_SMOOTH_LEVEL;
+            // Move along X
+            if( Math.abs(this.touchedPos.x - this.location.getX() - WIDTH/2) > Math.abs(this.speed.x * delta / (2*ACCEL_SMOOTH_LEVEL))) {
+                Float normWidth = MathUtils.clamp(
+                        this.location.getX() + ( direction * this.speed.x * delta / ACCEL_SMOOTH_LEVEL),
+                        0,
+                        Application.worldWidth - Player.WIDTH
+                );
+                this.location.setLocation(normWidth, this.location.getY());
+            }
+            else {
+                this.direction = 0;
+                this.speed.x = 0;
+            }
+            // Move along Y
+            this.location.add(0, - this.speed.y * delta);
         }
-        else {
-            this.direction = 0;
-            this.speed.x = 0;
-        }
-
-        // Move along Y
-        this.location.add(0, - this.speed.y * delta);
     }
 
     @Override
