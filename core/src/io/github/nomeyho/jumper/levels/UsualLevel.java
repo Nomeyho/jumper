7261ec37eac550eacda84898b0f891a5afc87e69
Stéphane Vanberg <svanberg@student.ulg.ac.be>
2017-07-31 21:46:19 +0100

    Remove transitions

--- core/src/io/github/nomeyho/jumper/levels/UsualLevel.java
+++ core/src/io/github/nomeyho/jumper/levels/UsualLevel.java
@@ -1,11 +1,7 @@
 package io.github.nomeyho.jumper.levels;
 
-import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Pool;
-import io.github.nomeyho.jumper.AbstractGame;
 import io.github.nomeyho.jumper.Application;
 import io.github.nomeyho.jumper.GameManager;
 import io.github.nomeyho.jumper.objects.AbstractGameObject;
